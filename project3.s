abc7bf0cdb864af04e7117b3ae849ccd834c2745
Wontha <wontha.ks@gmail.com>
2018-12-11 15:07:11 -0500

    Added more code to divide exponent after use and storing all the values back into stack

--- project3.s
+++ project3.s
@@ -230,7 +230,12 @@ calculateInteger:
 	addi $t2, $t2, -48	#subtract 48 from $t2 to make it so that integer 0 is equivalent to 0
 	mult $t2, $t5		#multiply value in $t2 by exponent
 	mflo $t0	#add contents of special register $LO to $t0 
-	add $t6, $t6, $t0	#add value of $t0 to sum register ($t6)	
+	add $t6, $t6, $t0	#add value of $t0 to sum register ($t6)
+	li $t0, 28			#load 28 into $t0 to use to divide exponent
+	div $t5, $t0		#divide exponent by 28 ($t5 / $t0)
+	mflo $t5	#then, move contents of $LO (quotient) into $t5
+	sw $t5, 0($sp)		#store exponent back into stack
+	sw $t6, 8($sp)		#store sum back into stack	
 	
 	lb $t2, 0($s1)			#load byte from stack (character) into $t2
 	li $t0, 32			#load 32 into $t0 to use to compare for space character
