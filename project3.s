62ff5867b918bc95b2490d28658e8197cb8fa500
Wontha <wontha.ks@gmail.com>
2018-12-12 15:04:17 -0800

    Started rewriting the calculateExponent subprogram to make it recursive as the previous implementation was not quite right

--- project3.s
+++ project3.s
@@ -158,10 +158,12 @@ exit:
     syscall
 
 calculateExponent:
-	sw $ra, 8($sp)			#save real return address into stack pointer
-calculateExponentMain:
-	li $t0, 1				#load 1 to compare string to
-	lw $t8, 0($sp)			#load length into $t8 register
+	addi $sp, $sp, -8 		#make space for values to store
+	sw $ra, 4($sp)			#save return address into stack pointer
+	sw $a0, 0($sp)			#save argument (length)
+	li $t0, 1				#store 1 in $t0 to compare with
+	
+	
 	beq $t8, $t0, calculateExponentBaseHandle		#if length of string is one, go back to return address
 	
 	li $t0, 28				#load base-N number to $t0 to calculate exponent (in this case base-28)
